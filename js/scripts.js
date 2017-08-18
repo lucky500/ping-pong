35335efd64e1f5bdb238eb77545bea237ab38506
Luciano Oliveira <luciano101@msn.com>
2017-08-18 09:38:32 -0700

    receives user input, pass through a for loop, return ping if number is divisible by 3, returns pong if the number is divisible by 5, return ping pong if the number is divisible by 3 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 //Business logic
-
+function pingPong(userInput){
+  if(userInput === " "){
+    alert("Please Enter a Number");
+  } else {
+    for(var i = 1; i <= userInput; i++){
+      if(i % 3 === 0 && i % 5 === 0){
+        console.log("ping pong");
+      } else if(i % 3 === 0){
+        console.log("ping");
+      } else if (i % 5 === 0){
+        console.log("pong");
+      } else {
+        console.log(i);
+      }
+    }
+  }
+}
 
 //User Interface logic
 $(document).ready(init);
@@ -8,6 +24,6 @@ function init(){
   $('form').submit(function(e){
     e.preventDefault();
     var userInput = $('#inputNumber').val();
-    console.log(userInput);
+    pingPong(userInput);
   });
 }
