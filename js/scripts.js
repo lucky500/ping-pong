f8828a17b73bcac9f109013afa055e33e7505810
Luciano Oliveira <luciano101@msn.com>
2017-08-18 11:31:35 -0700

    take pingPong function result save it to a variable, map trhough variable and add li tags, and output to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,39 @@
 //Business logic
 function pingPong(userInput){
+  var result = [];
   if(userInput === " "){
     alert("Please Enter a Number");
   } else {
     for(var i = 1; i <= userInput; i++){
-      if(i % 3 === 0 && i % 5 === 0){
-        console.log("ping pong");
-      } else if(i % 3 === 0){
-        console.log("ping");
-      } else if (i % 5 === 0){
-        console.log("pong");
-      } else {
-        console.log(i);
-      }
+      // if(i % 3 === 0 && i % 5 === 0){
+      //   console.log("ping pong");
+      // } else if(i % 3 === 0){
+      //   console.log("ping");
+      // } else if (i % 5 === 0){
+      //   console.log("pong");
+      // } else {
+      //   console.log(i);
+      // }
+      i % 3 === 0 && i % 5 === 0 ? result.push('pingpong') 
+      : i % 3 === 0 ? result.push('ping') 
+      : i % 5 === 0 ? result.push('pong') 
+      : result.push(i);
     }
-  }
+  } 
+  return result;
 }
+
 
 //User Interface logic
 $(document).ready(init);
-
 function init(){
   $('form').submit(function(e){
     e.preventDefault();
-    var userInput = $('#inputNumber').val();
-    pingPong(userInput);
+    var userInput = $('#inputNumber').val();   
+    var outputResult = pingPong(userInput);
+    var modifiedResult = outputResult.map(function(item){
+      return '<li>' + item + '</li>';
+    });
+    $('.output').html(modifiedResult.join(""));
   });
 }
